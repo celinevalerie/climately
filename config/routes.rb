7660040860adc6e0edcf31f93956f2f2fc453fdd
Lena <60652141+wwnwn@users.noreply.github.com>
2020-09-01 18:37:14 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   put '/users/:id', to: 'users#update', as: 'update_user'
 
