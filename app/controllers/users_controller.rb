b8c0df8d35cbd027959c5404c9cf803a907c4b7b
Celine Stalder <celine.stalder@student.unisg.ch>
2020-09-01 18:46:02 +0100

    signup fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :user_name, :email, :password, :photo)
+    params.require(:user).permit(:first_name, :last_name, :user_name, :photo)
   end
 end
