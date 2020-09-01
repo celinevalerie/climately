239ccfc4a6dd7a484a0faa1f255a25d5e1aecb7c
Lena <60652141+wwnwn@users.noreply.github.com>
2020-09-01 16:43:59 +0200

    update validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,8 @@ class Group < ApplicationRecord
   belongs_to :challenge, dependent: :destroy
   has_many :groups, through: :users_groups
 
-    validates :completed, inclusion: { in:[true, false] }
-  validates:difficulty, :impact, :duration, :exceptions, presence: true
-  validates_inclusion_of :difficulty, :in => 1..10
+    
+  validates :difficulty, :impact, :duration, :exceptions, :completed,presence: true
+  validates :difficulty, :inclusion => 1..10
+  validates :completed, inclusion: [true, false]
 end
