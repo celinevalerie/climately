f7cbef2fb5409f21945da1593e75e2de097f695e
Lena <60652141+wwnwn@users.noreply.github.com>
2020-09-02 11:56:35 +0200

    Update and rename 20200901154328_add_omniauth_to_users.rb to 20200901154328_add_omni_to_users.rb

--- db/migrate/20200901154328_add_omni_to_users.rb
+++ db/migrate/20200901154328_add_omni_to_users.rb
@@ -1,4 +1,4 @@
-class AddOmniauthToUsers < ActiveRecord::Migration[6.0]
+class AddOmniToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :provider, :string
     add_column :users, :uid, :string
