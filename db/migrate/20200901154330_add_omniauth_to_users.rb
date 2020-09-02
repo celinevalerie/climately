220791f1d4228efaac84708c80f8e14f753e4449
Lena <60652141+wwnwn@users.noreply.github.com>
2020-09-02 13:38:05 +0200

    Delete 20200901154330_add_omniauth_to_users.rb

--- db/migrate/20200901154330_add_omniauth_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddOmniauthToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :facebook_picture_url, :string
-    add_column :users, :token, :string
-    add_column :users, :token_expiry, :datetime
-  end
-end
