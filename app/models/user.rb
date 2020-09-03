704b615a5a11992ae7604e98bb202b26857cb959
Samira Eilinger <samira_eilinger@hotmail.com>
2020-09-03 23:03:47 +0100

    small stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :user_groups
+  has_many :messages
   has_many :groups, through: :user_groups
   has_many :friendships
   has_many :friends, through: :friendships
--- app/policies/chatroom_policy.rb
+++ app/policies/chatroom_policy.rb
@@ -1,14 +1,14 @@
 class ChatroomPolicy < ApplicationPolicy
 
   def show? 
-    @group = Group.where("chat_id = #{record.id}")
+    @group = record.group
     @group.users.include?(user) or user.admin?
   end
 
   def index? 
     return true
   end
-  
+
   class Scope < Scope
     def resolve
       scope.all
