9086fef5fb0d7030819c7a84373f2cfcb48fab62
Celine Stalder <celine.stalder@student.unisg.ch>
2020-09-07 19:52:12 +0100

    slider calculation

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = [ 'difficulty' ];
+  static targets = [ 'difficulty', 'exceptions', 'impact', 'duration' ];
 
   connect() {
     console.log(this.difficultyTarget);
@@ -10,7 +10,19 @@ export default class extends Controller {
     fetch('/challenges', { headers: { accept: "application/json" } })
       .then(response => response.json())
       .then((data) => {
-        console.log(this.difficultyTarget);
+        const challenge = data.challenges.find(challenge => challenge.id == event.target.dataset.id);
+        var difficultyCalculated = challenge.default_difficulty - this.exceptionsTarget.value
+        this.difficultyTarget.style.width = (difficultyCalculated * 30) +"px"
       });
+  }
+   calculateImpact(event) {
+      fetch('/challenges', { headers: { accept: "application/json" } })
+        .then(response => response.json())
+        .then((data) => {
+          const challenge = data.challenges.find(challenge => challenge.id == event.target.dataset.id);
+          var impactCalculated = challenge.default_impact * this.durationTarget.value
+          this.impactTarget.style.width = (impactCalculated * 3) +"px"
+          console.log(this.durationTarget.style.width)
+        });
   }
 }
